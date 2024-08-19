d9e9597c20cae819a0960ff890e1a6c4fa3f6c6d
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-19 23:59:09 +0000

    simple shell

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*header files*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/stat.h>
+
+
+#endif /* MAIN_H */
