c271d02d6601fbbf7fd232366902148c8466cd8a
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-20 03:09:55 +0000

    file 2

---
+++ hsh2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main(void) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char *delimeter = "\t\n";
+    while (1) {
+        printf("SimpleShell$ ");
+        nread = getline(&line, &len, stdin);
+        if (nread == -1) {
+            perror("Exiting shell");
+            exit(1);
+        }
+        // Tokenize input
+        char *token;
+        char **command = NULL;
+       	int i = 0;
+
+        while ((token = strtok_r(line, delimeter, &line))) {
+            command = realloc(command, (i + 1) * sizeof(char *));
+            if (!command) {
+                perror("Error: Memory allocation failed");
+                exit(1);
+            }
+            command[i++] = token;
+        }
+        command[i] = NULL;
+        pid_t child_pid = fork();
+        if (child_pid == 0) {
+            if (execvp(command[0], command) == -1) {
+                perror("Could not execute");
+                exit(1);
+            }
+        } else {
+            wait(NULL);
+        }
+    }
+    // Cleanup
+    
+    free(line);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
