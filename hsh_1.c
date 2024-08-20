f66a071d6fbeafd29c68f2913743cf63fecac92f
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-20 03:21:59 +0000

    simple shell

--- hsh_1.c
+++ hsh_1.c
@@ -6,22 +6,26 @@
 
 int main(void)
 {
+	pid_t child_pid;
+	char *token;
+	char **command = NULL;
     char *line = NULL;
     size_t len = 0;
     ssize_t nread;
     char *delimeter = "\t\n";
-    while (1) {
+    int i;
+    
+    	while (1) {
         printf("SimpleShell$ ");
         nread = getline(&line, &len, stdin);
         if (nread == -1) {
             perror("Exiting shell");
             exit(1);
         }
- 
-        char *token;
-        char **command = NULL;
-        int i = 0;
-        while ((token = strtok_r(line, delimeter, &line))) {
+
+        i = 0;
+        
+	while ((token = strtok_r(line, delimeter, &line))) {
             command = realloc(command, (i + 1) * sizeof(char *));
             if (!command) {
                 perror("Error: Memory allocation failed");
@@ -30,7 +34,7 @@ int main(void)
             command[i++] = token;
         }
         command[i] = NULL;
-        pid_t child_pid = fork();
+        child_pid = fork();
         if (child_pid == 0) {
             if (execvp(command[0], command) == -1) {
                 perror("Could not execute");
