782cfcd10142280d4e99506784810cd990776ccf
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-21 00:10:34 +0000

    helper

--- helper.c
+++ helper.c
@@ -1,18 +1,10 @@
 #include "main.h"
 /**
- * _printf - ...
- */
-void _printf(char *prompt)
-{
-	write(STDOUT_FILENO, prompt, strlen(prompt));
-}
-/**
  * display_prompt - ...
  */
-
 void display_prompt(void)
 {
-	_printf("SimpleShell$ ");
+	write(STDOUT_FILENO, "SimpleShell$ ", 13);
 }
 /*
  * read_command- ...
