8281e94052aeb572bff3c6377273e929e70c7e5c
phon xay <majic42@hotmail.com>
2024-08-22 06:44:39 +0000

    add phon helper functions

---
+++ helper_p.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculate the length of a string
+ * @s: The string to measure
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcpy - Copy a string
+ * @dest: Destination buffer
+ * @src: Source string
+ *
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strcat - Concatenate two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: Pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = _strlen(dest);
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
