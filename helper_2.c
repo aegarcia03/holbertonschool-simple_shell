0745d817764d19e17ea90de8287bc1a834e7fd27
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-22 01:11:33 +0000

    environ

--- helper_2.c
+++ helper_2.c
@@ -7,4 +7,17 @@ void sigint_handler(int sig)
 	(void)sig;
 	write(STDOUT_FILENO, "\nSimpleShell$ ", 14);
 }
+/*
+ * print_env - ...
+ */
+void print_env(void)
+{
+	char **env = environ;
 
+	while (*env)
+	{
+		write(STDOUT_FILENO, *env, strlen(*env));
+		write(STDOUT_FILENO, "\n", 1);
+		env++;
+	}
+}
