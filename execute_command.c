00bd2800cf35fea3066a9d7469dadbc219c56714
phon xay <majic42@hotmail.com>
2024-08-22 13:02:02 +0000

    fix betty style

--- execute_command.c
+++ execute_command.c
@@ -1,55 +1,42 @@
 #include "main.h"
 /**
- * execute_command - ...
+ * execute_command - run the command found
+ * @command: command entered
  */
 void execute_command(char **command)
 {
-    pid_t child_pid;
-    int status;
-    char *full_path;
+	pid_t child_pid;
+	int status;
+	char *full_path;
 
-    if (command[0] == NULL)
-    {
-        return;
-    }
-
-
-    if (command[0][0] == '/' || command[0][0] == '.')
-    {
-
-        full_path = strdup(command[0]);
-    }
-    else
-    {
-
-        full_path = find_path(command[0]);
-        if (full_path == NULL)
-        {
-            fprintf(stderr, "./hsh: 1: %s: not found\n", command[0]);
-            return;
-        }
-    }
-
-    child_pid = fork();
-    if (child_pid == -1)
-    {
-        perror("Error: Fork failed");
-        exit(1);
-    }
-
-    if (child_pid == 0)
-    {
-        if (execve(full_path, command, environ) == -1)
-        {
-            perror("Error: Command execution failed");
-            exit(1);
-        }
-    }
-    else
-    {
-        wait(&status);
-    }
-
-    free(full_path);
+	if (command[0] == NULL)
+		return;
+	if (command[0][0] == '/' || command[0][0] == '.')
+		full_path = strdup(command[0]);
+	else
+	{
+		full_path = find_path(command[0]);
+		if (full_path == NULL)
+		{
+			fprintf(stderr, "./hsh: 1: %s: not found\n", command[0]);
+			return;
+		}
+	}
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error: Fork failed");
+		exit(1);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(full_path, command, environ) == -1)
+		{
+			perror("Error: Command execution failed");
+			exit(1);
+		}
+	}
+	else
+		wait(&status);
+	free(full_path);
 }
-
